3b2a85f694079dbf22f5562d58678c7044583845
mrojek-gr <mrojek@griddynamics.com>
2025-07-01 14:04:14 +0200

    basic configurations, templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:id', component: UserComponent },
+];
